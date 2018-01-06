c2b7d56d27916754eead334a016b8c149c67ee72
oded <odedmoran222@gmail.com>
2018-01-06 20:57:33 +0200

    add mongo + user model

--- server/server.js
+++ server/server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
-const clientPath = '/../client/';
 const apiRoutes = express.Router();
 const bodyParser = require('body-parser');
 const router = require('./router');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.use(express.static(path.join(__dirname, clientPath)));
-app.use('/scripts', express.static(path.join(__dirname, clientPath + 'node_modules')));
+const User = require('./models/User');
+
+// const clientPath = '/../client/';
+// app.use(express.static(path.join(__dirname, clientPath)));
+// app.use('/scripts', express.static(path.join(__dirname, clientPath + 'node_modules')));
+
+app.use(express.static(path.join(__dirname)));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,8 +25,25 @@ app.use(cors());
 //   res.sendFile(path.join(__dirname, clientPath + 'index.html'));
 // });
 
+app.post('/register', (req, res) => {
+  const userData = req.body;
+  const user = new User(userData);
+  user.save((err, result) => {
+    if (err) {
+      console.log('saving user error');
+    }
+    res.sendStatus(200);
+  });
+});
+
 app.listen(port, function () {
   console.log('listening on port ' + port); 
+});
+
+mongoose.connect('mongodb://pilates:password@ds237475.mlab.com:37475/pilates', (err) => {
+  if (!err) {
+    console.log('connected to mongo');
+  }
 });
 
 router(app);
